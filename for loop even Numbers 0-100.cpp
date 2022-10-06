03196123aa23905fcfba87c474a33e7da3da259e
isalamba01 <113933926+isalamba01@users.noreply.github.com>
2022-10-06 10:42:59 +0300

    Add files via upload

---
+++ for loop even Numbers 0-100.cpp
@@ -0,0 +1,13 @@
+// program using the for loop to display even numbers between 0-100
+#include <iostream>
+using namespace std;
+int main ()
+{
+//variable declaration
+int i;
+for (i=2;i<=100;i=i+2)
+{
+    cout <<i<<" ";
+}
+return 0;
+}
